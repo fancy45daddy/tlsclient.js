1cac4c1f83e98873d126f200a2eaa6387aa1361d
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2024-04-14 17:37:53 -0700

    Update adapter.mjs

--- helpers/adapter.mjs
+++ helpers/adapter.mjs
@@ -9,17 +9,15 @@ let { TLS_LIB_PATH } = getTLSDependencyPath();
 let DEFAULT_CLIENT_ID = "chrome_120";
 let DEFAULT_HEADERS = {
   accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-  "accept-encoding": "gzip, deflate, br",
-  "accept-language": "en-US,en;q=0.9",
-  "sec-ch-ua": `"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"`,
+  "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+  "sec-ch-ua": `"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"`,
   "sec-ch-ua-mobile": "?0",
-  "sec-ch-ua-platform": '"Windows"',
+  "sec-ch-ua-platform": '"Linux"',
   "sec-fetch-dest": "document",
   "sec-fetch-mode": "navigate",
   "sec-fetch-site": "same-origin",
   "sec-fetch-user": "?1",
-  "user-agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+  "user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
 };
 let DEFAULT_HEADER_ORDER = [
   "host",
