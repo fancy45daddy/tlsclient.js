c9e417ff1c15fa6bedafde1e1e1f5d31353af65b
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2024-04-17 15:35:11 -0700

    Update adapter.mjs

--- helpers/adapter.mjs
+++ helpers/adapter.mjs
@@ -10,15 +10,19 @@ let DEFAULT_CLIENT_ID = "chrome_120";
 let DEFAULT_HEADERS = {
   accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
   "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+  'dpr':'1',
+  'sec-ch-prefers-color-scheme':'light',
   "sec-ch-ua": `"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"`,
   "sec-ch-ua-full-version-list": `"Google Chrome";v="123.0.6312.122", "Not:A-Brand";v="8.0.0.0", "Chromium";v="123.0.6312.122"`,
   "sec-ch-ua-mobile": "?0",
   'sec-ch-ua-model': '""',
   "sec-ch-ua-platform": '"Linux"',
+  'sec-ch-ua-platform-version': '"5.15.0"',
   "sec-fetch-dest": "document",
   "sec-fetch-mode": "navigate",
-  "sec-fetch-site": "same-origin",
+  "sec-fetch-site": "none",
   "sec-fetch-user": "?1",
+  'upgrade-insecure-requests':'1',
   "user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
 };
 let DEFAULT_HEADER_ORDER = [
